86f35b0fac1bab382ae256460317018e2a05e5a4
Priya <priyanemade4@gmail.com>
2020-02-25 19:39:43 +0530

    added testclass 2

--- src/TestCase1.java
+++ src/TestCase1.java
@@ -11,14 +11,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class TestCase1 {
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
+		/**WebDriverManager.chromedriver().setup();
 		RemoteWebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.eagercrow.com");
 		Thread.sleep(5000);
 		//department
-		driver.findElement(By.xpath("(//a[@class='color0-hov'])[1]")).click();
-
+		driver.findElement(By.xpath("(//a[@class='color0-hov'])[1]")).click();*/
+System.out.println("Hello");
 		
 		
 		
